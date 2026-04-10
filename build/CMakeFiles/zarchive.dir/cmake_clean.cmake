87c37aee51b22f5f9bdec552e792090515784a1f
Sayora <36930070+Sayora77@users.noreply.github.com>
2026-04-10 17:07:08 -0400

    a

---
+++ build/CMakeFiles/zarchive.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zarchive.dir/src/sha_256.c.obj"
+  "CMakeFiles/zarchive.dir/src/sha_256.c.obj.d"
+  "CMakeFiles/zarchive.dir/src/zarchivereader.cpp.obj"
+  "CMakeFiles/zarchive.dir/src/zarchivereader.cpp.obj.d"
+  "CMakeFiles/zarchive.dir/src/zarchivewriter.cpp.obj"
+  "CMakeFiles/zarchive.dir/src/zarchivewriter.cpp.obj.d"
+  "libzarchive.a"
+  "libzarchive.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/zarchive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
